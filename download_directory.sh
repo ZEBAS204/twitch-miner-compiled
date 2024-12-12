9b39f33e733a4ba602aa012445bb9fa4446fe366
SEBAS204 <36385457+ZEBAS204@users.noreply.github.com>
2024-12-12 19:38:51 +0000

    Also download project requirements.txt

--- download_directory.sh
+++ download_directory.sh
@@ -6,6 +6,21 @@
 # The original URL will be https://github.com/rdavydov/Twitch-Channel-Points-Miner-v2
 repo="rdavydov/Twitch-Channel-Points-Miner-v2"
 
+# Create the tmp directory if it doesn't exist
+mkdir -p tmp
+
 # This script will download the repository as a tar file and extract the folder
 # "TwitchChannelPointsMiner" automatically into the root directory.
-curl -L https://api.github.com/repos/${repo}/tarball | tar -xz --wildcards "*/TwitchChannelPointsMiner" --strip-components=1
+curl -L https://api.github.com/repos/${repo}/tarball | tar -xz -C tmp --wildcards \
+    "*/TwitchChannelPointsMiner" \
+    "*/requirements.txt" \
+    --strip-components=1
+
+# Move requirements.txt into TwitchChannelPointsMiner
+mv tmp/requirements.txt tmp/TwitchChannelPointsMiner/
+
+# Finally move the TwitchChannelPointsMiner folder into the root directory
+mv tmp/TwitchChannelPointsMiner .
+
+# Clean up
+rm -rf tmp
